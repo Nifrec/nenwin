41b88854de609f7d1a2ab0d4f5c54a98e2e38254
nifrec <l.l.piree@student.tue.nl>
2020-10-06 19:02:12 +0200

    Small teaks and fixes

--- src/particle.py
+++ src/particle.py
@@ -4,13 +4,17 @@ the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 
 Author: Lulof Pirée
 
-Most fundamental classes for building Nenwin-networks.
+Most fundamental pieces for building Nenwin-networks.
 """
 import abc
 import torch
 
 
 class Particle(abc.ABC):
+    """
+    Abstract Base Class for all particles of the simulation,
+    both the data-particles and the nodes of the model itself.
+    """
 
     def __init__(self,
                  pos: torch.Tensor,
@@ -32,19 +36,22 @@ class Particle(abc.ABC):
     def acc():
         return self.__acc
 
-    def set_pos(new_pos: torch.Tensor):
-        if (new_pos.shape !=self.__pos.shape):
+    @pos.setter
+    def pos(new_pos: torch.Tensor):
+        if (new_pos.shape != self.__pos.shape):
             raise RuntimeError("New position particle has different dimension")
         self.__pos = new_pos
 
-    def set_acc(new_acc: torch.Tensor):
-        if (new_acc.shape !=self.__acc.shape):
+    @acc.setter
+    def acc(new_acc: torch.Tensor):
+        if (new_acc.shape != self.__acc.shape):
             raise RuntimeError(
                 "New acceleration particle has different dimension")
-        self.__pos = new_pos
+        self.__acc = new_acc
 
-    def set_vel(new_vel: torch.Tensor):
-        if (new_vel.shape !=self.__vel.shape):
+    @vel.setter
+    def vel(new_vel: torch.Tensor):
+        if (new_vel.shape != self.__vel.shape):
             raise RuntimeError("New velocity particle has different dimension")
         self.__vel = new_vel
 
