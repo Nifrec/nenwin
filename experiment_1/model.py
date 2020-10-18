8c91552551e4d0a3b515315239a5cc055d966ab7
nifrec <l.l.piree@student.tue.nl>
2020-10-18 22:11:56 +0200

    Update NenwinModel interface

--- experiment_1/model.py
+++ experiment_1/model.py
@@ -9,26 +9,51 @@ Class to hold pieces of network together, perform timesteps and manage i/o
 around the simulation.
 """
 import numpy as np
-from typing import Iterable
+from typing import Set, Iterable, Optional
 import multiprocessing
+from numbers import Number
 
 from experiment_1.particle import PhysicalParticle
 from experiment_1.node import Node
+from experiment_1.marble import Marble
+
 
 class NenwinModel():
     """
     Class representing the simulation: keeps track of the nodes, marbles,
     input, output and advances timesteps.
     """
-    
-    def __init__(self, nodes: Iterable[Node], step_size: float):
+
+    def __init__(self,
+                 nodes: Iterable[Node],
+                 step_size: float,
+                 initial_marbles: Optional[Set[Marble]] = None):
         """
         Set up the model, given a number of Node instances,
         and a step-size for the simulation.
+        Optionally some input Marble's can already be added, 
+        which will move as soon as NenwinModel.run() is called.
         """
-        self.__nodes = nodes
+        self.__nodes = set(nodes)
+        self.__marbles = set()
         self.__step_size = step_size
         self.__queue = multiprocessing.Queue()
+
+    @property
+    def nodes(self) -> Set[Node]:
+        """
+        Get the set of all currently simulated nodes, 
+        each in their current state.
+        """
+        return self.__nodes.copy()
+
+    @property
+    def marbles(self) -> Set[Marble]:
+        """
+        Get the set of all currently simulated marbles, 
+        each in their current state.
+        """
+        return self.__marbles.copy()
 
     @property
     def queue(self):
@@ -43,5 +68,17 @@ class NenwinModel():
     def __produce_outputs(self):
         pass
 
-    def run(self):
-        pass
+    def run(self, max_num_steps:Number=float("inf")):
+        """
+        Start simulation and imput processing until stop signal is received.
+        While running, will accept inputs, and produce outputs when requested.
+
+        By default runs indefinitely until stop signal is received.
+        An optional max amount of timesteps can be given
+        (convenient for testing).
+        """
+        num_remaining_steps = max_num_steps
+
+        while num_remaining_steps > 0:
+            num_remaining_steps -= 1
+
