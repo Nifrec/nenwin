0e4b59cdf790855aa2ce3a5cd41a38aeb2cdd924
nifrec <l.l.piree@student.tue.nl>
2020-10-24 21:17:37 +0200

    Implement MarbleEaterNode

--- experiment_1/node.py
+++ experiment_1/node.py
@@ -12,6 +12,7 @@ They themselves may or may not be attracted by the marbles as well.
 from __future__ import annotations
 import numpy as np
 from experiment_1.particle import PhysicalParticle
+from experiment_1.marble import Marble
 
 class Node(PhysicalParticle):
     """
@@ -53,3 +54,41 @@ class Node(PhysicalParticle):
         if (self.__stiffness != 1):
             forces = (1 - self.__stiffness) * forces
             super().update_acceleration(forces)
+
+class MarbleEaterNode(Node):
+    """
+    A node variant that consumes Marbles when they become within
+    a certain radius of the Eater.
+    Keeps track of total count of Marbles eater,
+    and the data fields of the Marbles it ate.
+    """
+
+    def __init__(self,
+                pos: np.ndarray,
+                 vel: np.ndarray,
+                 acc: np.ndarray,
+                 mass: float,
+                 attraction_function: callable,
+                 stiffness: float,
+                 radius: float):
+        super().__init__(pos, vel, acc, mass, attraction_function, stiffness)
+        self.__radius = radius
+        self.__num_marbles_eaten: int = 0
+        self.__marble_data_eaten = []
+
+    @property
+    def radius(self) -> float:
+        return self.__radius
+
+    @property
+    def num_marbles_eaten(self) -> int:
+        return self.__num_marbles_eaten
+
+    @property
+    def marble_data_eaten(self) -> List[object]:
+        return self.__marble_data_eaten.copy()
+
+    def eat(self, marble: Marble):
+        self.__num_marbles_eaten += 1
+        self.__marble_data_eaten.append(marble.datum)
+        
--- test/node_test.py
+++ test/node_test.py
@@ -11,7 +11,8 @@ import unittest
 import numpy as np
 from typing import Tuple
 
-from experiment_1.node import Node
+from experiment_1.node import Node, MarbleEaterNode
+from experiment_1.marble import Marble
 from experiment_1.attraction_functions.attraction_functions import Gratan
 from test_aux import check_close
 
@@ -86,7 +87,7 @@ class NodeTestCase(unittest.TestCase):
         stiffness = 0
         node = Node(pos, vel, acc, mass, lambda: None, stiffness)
 
-        forces=np.array([[1, 1, 1]])
+        forces = np.array([[1, 1, 1]])
         node.update_acceleration(forces)
 
         expected = np.array([1, 1, 1])
@@ -104,10 +105,86 @@ class NodeTestCase(unittest.TestCase):
         stiffness = 1
         node = Node(pos, vel, acc, mass, lambda: None, stiffness)
 
-        forces=np.array([[1, 1, 1]])
+        forces = np.array([[1, 1, 1]])
         node.update_acceleration(forces)
 
         self.assertTrue(check_close(node.acc, acc))
+
+
+class MarbleEaterNodeTestCase(unittest.TestCase):
+
+    def test_eat_1(self):
+        """
+        Base case: eat a Marble, test if stored correctly.
+        """
+        # Irrelevant parameters
+        pos = np.array([1, 3, 2])
+        vel = np.array([1, 1, 1])
+        acc = np.array([10, 9, 0])
+        mass = 1
+        stiffness = 0.5
+        radius = 0
+
+        node = MarbleEaterNode(pos, vel, acc, mass, lambda: None,
+                               stiffness, radius)
+        datum = "Hell0 W0rld!"
+        marble = Marble(pos, vel, acc, mass, lambda: None, datum)
+
+        node.eat(marble)
+        del marble # It is supposed to dissapear now in the Simulation as well.
+
+        self.assertEqual(node.num_marbles_eaten, 1)
+        self.assertListEqual(node.marble_data_eaten, [datum])
+
+    def test_eat_2(self):
+        """
+        Corner case: no Marbles eaten.
+        """
+        # Irrelevant parameters
+        pos = np.array([1, 3, 2])
+        vel = np.array([1, 1, 1])
+        acc = np.array([10, 9, 0])
+        mass = 1
+        stiffness = 0.5
+        radius = 0
+
+        node = MarbleEaterNode(pos, vel, acc, mass, lambda: None,
+                               stiffness, radius)
+
+        self.assertEqual(node.num_marbles_eaten, 0)
+        self.assertListEqual(node.marble_data_eaten, [])
+
+    def test_eat_2(self):
+        """
+        Base case: eat 2 Marbles, test if stored correctly in right order.
+        """
+        # Irrelevant parameters
+        pos = np.array([1, 3, 2])
+        vel = np.array([1, 1, 1])
+        acc = np.array([10, 9, 0])
+        mass = 1
+        stiffness = 0.5
+        radius = 0
+
+        node = MarbleEaterNode(pos, vel, acc, mass, lambda: None,
+                               stiffness, radius)
+        datum1 = "Hell0 W0rld!"
+        marble1 = Marble(pos, vel, acc, mass, lambda: None, datum1)
+
+        node.eat(marble1)
+        del marble1 # It is supposed to dissapear now in the Simulation as well.
+
+        datum2 = set([1, 2, 3])
+        marble2 = Marble(pos, vel, acc, mass, lambda: None, datum2)
+        node.eat(marble2)
+        del marble2
+
+        self.assertEqual(node.num_marbles_eaten, 2)
+        self.assertListEqual(node.marble_data_eaten, [datum1, datum2])
+
+
+
+
 
 
 if __name__ == '__main__':
