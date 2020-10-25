b5a4dba7c872d55275a21967c021f395790befd3
nifrec <l.l.piree@student.tue.nl>
2020-10-25 22:16:42 +0100

    Add extended licence header

--- test/num_eaten_model_test.py
+++ test/num_eaten_model_test.py
@@ -2,6 +2,21 @@
 Nenwin-project (NEural Networks WIthout Neurons) for
 the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 
+Copyright (C) 2020 Lulof Pirée, Teun Schilperoort
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 Author: Lulof Pirée
 October 2020
 
