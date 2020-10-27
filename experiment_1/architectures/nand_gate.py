a3567afc82449db996fdd480a3dfcf9f138e5e6c
nifrec <l.l.piree@student.tue.nl>
2020-10-27 22:02:38 +0100

    Build Nenwin architecture to simulate NAND-gate

--- experiment_1/architectures/nand_gate.py
+++ experiment_1/architectures/nand_gate.py
@@ -23,6 +23,7 @@ October 2020
 Simulation of NAND-gates using Nenwin architectures.
 """
 import numpy as np
+from typing import Tuple
 
 from experiment_1.model import NumMarblesEatenAsOutputModel
 from experiment_1.node import Node, MarbleEaterNode
@@ -31,46 +32,61 @@ from experiment_1.attraction_functions.attraction_functions import Gratan
 from experiment_1.attraction_functions.attraction_functions import NewtonianGravity
 from experiment_1.visualization import NenwinVisualization
 
-BOOL_TO_MASS = {True: 10, False: -10}
-
+BOOL_TO_MASS = {True: 100, False: -100}
+ZERO = np.array([0, 0])
+ATTRACTION_FUNCTION = NewtonianGravity()
 
 def nand_gate(input_1: bool, input_2: bool) -> bool:
     """
     Simulate a NAND gate using a Nenwin architecture.
     Also visualizes the simulation and prints the result.
     """
-    attraction_function = NewtonianGravity()
-    zero = np.array([0, 0])
-    eater = MarbleEaterNode(pos=np.array([10, 20]),
-                            vel=zero,
-                            acc=zero,
-                            mass=10,
-                            stiffness=1,
-                            attraction_function=attraction_function,
-                            radius=5)
+    print(f"NAND({input_1}, {input_2})")
+    print("Press ESC after the simulation ended to print NAND gate output")
+    marble1, marble2 = __generate_nand_marbles(input_1, input_2)
+    eater, wastebin_eater = __generate_nand_nodes()
+    model = NumMarblesEatenAsOutputModel([eater, wastebin_eater],
+                                         0.001,
+                                         [marble1, marble2],)
+    visualization = NenwinVisualization((500, 500), model, 10)
+    visualization.run(10)
+    output = model._produce_outputs()
 
-    node = Node(pos=np.array([30, 20]),
-                vel=zero,
-                acc=zero,
-                mass=-10,
-                stiffness=1,
-                attraction_function=attraction_function)
+    print("NAND output:")
+    if output[0] == 2:
+        print(False)
+    else:
+        print(True)
 
+def __generate_nand_marbles(input_1: bool, input_2: bool) -> Tuple[Marble]:
     marble1 = Marble(np.array([20, 10]),
-                     vel=zero, acc=zero, mass=BOOL_TO_MASS[input_1],
-                     attraction_function=attraction_function,
+                     vel=ZERO, acc=ZERO, mass=BOOL_TO_MASS[input_1],
+                     attraction_function=ATTRACTION_FUNCTION,
                      datum=input_1)
 
     marble2 = Marble(np.array([20, 30]),
-                     vel=zero, acc=zero, mass=BOOL_TO_MASS[input_2],
-                     attraction_function=attraction_function,
+                     vel=ZERO, acc=ZERO, mass=BOOL_TO_MASS[input_2],
+                     attraction_function=ATTRACTION_FUNCTION,
                      datum=input_2)
+    return marble1, marble2
 
-    model = NumMarblesEatenAsOutputModel([eater, node],
-                                         0.01,
-                                         [marble1, marble2])
-    visualization = NenwinVisualization((500, 500), model)
-    visualization.run(10)
+def __generate_nand_nodes() -> Tuple[MarbleEaterNode]:
+    eater = MarbleEaterNode(pos=np.array([5, 20]),
+                            vel=ZERO,
+                            acc=ZERO,
+                            mass=10,
+                            stiffness=1,
+                            attraction_function=ATTRACTION_FUNCTION,
+                            radius=5)
+
+    wastebin_eater = MarbleEaterNode(pos=np.array([35, 20]),
+                                     vel=ZERO,
+                                     acc=ZERO,
+                                     mass=-10,
+                                     stiffness=1,
+                                     attraction_function=ATTRACTION_FUNCTION,
+                                     radius=5)
+    return eater, wastebin_eater
 
 
 if __name__ == "__main__":
