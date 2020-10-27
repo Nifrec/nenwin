bfb1d21d802d136f73bdd5473dfc1ffc935e8912
nifrec <l.l.piree@student.tue.nl>
2020-10-27 22:01:39 +0100

    Improve radius rendering

--- experiment_1/visualization.py
+++ experiment_1/visualization.py
@@ -7,21 +7,24 @@ October 2020
 
 Simple graphical visualization of a running simulation.
 """
-import numpy as np
-from typing import Tuple
+
 from os import environ
 # Disable pygame welcome message. Need to be set before 'import pygame'
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
+import numpy as np
+from typing import Tuple
 
 from experiment_1.model import NenwinModel
+from experiment_1.node import Node, MarbleEaterNode
 
 
 
 NODE_COLOUR = pygame.Color(255, 143, 95)
 MARBLE_COLOUR = pygame.Color(155, 255, 95)
 BACKGROUND_COLOR = pygame.Color(225, 201, 255)
-RADIUS_SCALE_FACTOR = 0.1
+NODE_RADIUS = 10
+MARBLE_RADIUS = 5
 
 
 class NenwinVisualization():
@@ -31,19 +34,25 @@ class NenwinVisualization():
     Note that is a simple, naive low-performance implementation.
     """
 
-    def __init__(self, resolution: Tuple[int], model: NenwinModel):
+    def __init__(self,
+                 resolution: Tuple[int],
+                 model: NenwinModel,
+                 scale_factor: int = 1):
         """
         Arguments:
         * resolution: (width, height) of window. Note that the simulation
             is not scaled to fit in the resolution, so not all particles
             might be visible for a small resolution.
         * model: the NenwinModel to visualize. The model should not be running.
+        * scale_factor: factor for scaling coordinates of objects (by default
+            position directly correspond to pixel coordinates)
         """
         pygame.init()
         pygame.display.set_caption("Nenwin")
         self.__resolution = resolution
         self.__model = model
         self.__screen = pygame.display.set_mode((resolution[0], resolution[1]))
+        self.__scale_factor = scale_factor
 
     def __draw_all_particles(self):
         new_frame = self.__create_new_frame()
@@ -58,17 +67,29 @@ class NenwinVisualization():
         surf = pygame.Surface(self.__resolution)
         surf.fill(BACKGROUND_COLOR)
         for node in self.__model.nodes:
+            radius = self.__find_radius_of_node(node)
             pygame.draw.circle(surf,
                                NODE_COLOUR,
-                               np.round(node.pos).astype(int),
-                               round(abs(node.mass) * RADIUS_SCALE_FACTOR))
+                               np.round(self.__scale_factor*node.pos).astype(int),
+                               np.round(self.__scale_factor*radius))
         for marble in self.__model.marbles:
             pygame.draw.circle(surf,
                                MARBLE_COLOUR,
-                               np.round(marble.pos).astype(int),
-                               round(abs(marble.mass) * RADIUS_SCALE_FACTOR))
-        
+                               np.round(self.__scale_factor*marble.pos).astype(int),
+                               MARBLE_RADIUS)
+
         return surf
+
+    def __find_radius_of_node(self, node: Node) -> float:
+        """
+        Return default radius of nodes if an ordinairy node
+        (without any 'radius' attribute),
+        or use the specified radius in case of an Eater.
+        """
+        if isinstance(node, MarbleEaterNode):
+            return node.radius
+        else:
+            return NODE_RADIUS
 
     def __process_events(self):
         """
@@ -77,11 +98,11 @@ class NenwinVisualization():
         # Now process pygame events (keyboard keys or controller buttons)
         for event in pygame.event.get():
 
-            if (event.type == pygame.QUIT): # If user closes pygame window
+            if (event.type == pygame.QUIT):  # If user closes pygame window
                 self.__is_running = False
-            
-            elif (event.type == pygame.KEYDOWN): # If a button is pressed
-                if (event.key == pygame.K_ESCAPE): # User pressed ESC
+
+            elif (event.type == pygame.KEYDOWN):  # If a button is pressed
+                if (event.key == pygame.K_ESCAPE):  # User pressed ESC
                     self.__is_running = False
 
     def run(self, steps_per_display_update: int):
