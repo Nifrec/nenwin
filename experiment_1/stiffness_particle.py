f90c22c61186ed62b31b6069fa922b3a1c5460aa
nifrec <l.l.piree@student.tue.nl>
2020-11-06 20:30:34 +0100

    Test and implement attraction functions of StiffnessParticle

---
+++ experiment_1/stiffness_particle.py
@@ -0,0 +1,77 @@
+"""
+Nenwin-project (NEural Networks WIthout Neurons) for
+the AI Honors Academy track 2020-2021 at the TU Eindhoven.
+
+Author: Lulof Pirée
+October 2020
+
+Base class for Nodes and Marbles, subclass of PhysicalParticle.
+"""
+from __future__ import annotations
+import abc
+import numpy as np
+from typing import Union
+from experiment_1.node import Node
+from experiment_1.marble import Marble
+from experiment_1.particle import PhysicalParticle
+
+
+class StiffnessParticle(PhysicalParticle):
+    def __init__(self,
+                 pos: np.ndarray,
+                 vel: np.ndarray,
+                 acc: np.ndarray,
+                 mass: float,
+                 attraction_function: callable,
+                 marble_stiffness: float,
+                 node_stiffness: float,
+                 marble_attraction: float,
+                 node_attraction: float):
+        super().__init__(pos, vel, acc, mass, attraction_function)
+        raise_error_if_any_not_in_range((marble_stiffness, node_stiffness,
+                                         marble_attraction, node_attraction),
+                                        lower=0,
+                                        upper=1
+                                        )
+        self.__marble_stiffness = marble_stiffness
+        self.__node_stiffness = node_stiffness
+        self.__marble_attraction = marble_attraction
+        self.__node_attraction = node_attraction
+
+    @property
+    def marble_stiffness(self) -> float:
+        return self.__marble_stiffness
+
+    @property
+    def node_stiffness(self) -> float:
+        return self.__node_stiffness
+
+    @property
+    def marble_attraction(self) -> float:
+        return self.__marble_attraction
+
+    @property
+    def node_attraction(self) -> float:
+        return self.__node_attraction
+
+    def compute_attraction_force_to(
+            self, other: Union[Marble, Node]) -> np.ndarray:
+        """
+        Computes the force vector induced by this particle to the
+        [other] paricle at the position of the other particle.
+        """
+        if isinstance(other, Marble):
+            multiplier = self.marble_attraction
+        elif isinstance(other, Node):
+            multiplier = self.node_attraction
+        else:
+            raise ValueError("Expected Node or Marble as [other]")
+
+        return multiplier * super().compute_attraction_force_to(other)
+
+
+def raise_error_if_any_not_in_range(values: Iterable[float],
+                                    lower: float,
+                                    upper: float):
+    if any(x < lower or x > upper for x in values):
+        raise ValueError("Expected value in range[0, 1]")
--- test/stiffness_particle_test.py
+++ test/stiffness_particle_test.py
@@ -10,13 +10,18 @@ Unit-tests for StiffnessParticle of particle.py.
 import unittest
 import numpy as np
 
-from experiment_1.particle import StiffnessParticle
+from experiment_1.stiffness_particle import StiffnessParticle
 from experiment_1.attraction_functions.attraction_functions import Gratan
+from experiment_1.node import Node
+from experiment_1.marble import Marble
+from experiment_1.particle import PhysicalParticle
+from test_aux import TestAttractionFunction
 from test_aux import NUMERICAL_ABS_ACCURACY_REQUIRED
 from test_aux import check_close
 from test_aux import runge_kutta_4_step
 
 ZERO = np.array([0])
+ATTRACT_FUNCT = TestAttractionFunction()
 
 
 class StiffnessParticleTestCase(unittest.TestCase):
@@ -61,6 +66,61 @@ class StiffnessParticleTestCase(unittest.TestCase):
     def test_attraction_errors_4(self):
         self.assertRaises(ValueError, create_particle, ZERO, ZERO, ZERO, -0.1)
 
+    def test_attraction_node_1(self):
+        """
+        Check if the node_attraction multiplier is used to compute force to a
+        node.
+        """
+        node_attraction = 0.5
+        # Position -1 ensures it will be pulled towards '+' x-direction
+        node = Node(np.array([-1]), ZERO, ZERO, 0, None, 0)
+        particle = create_particle(0, 0, 0, node_attraction)
+
+        result = particle.compute_attraction_force_to(node)
+        self.assertEqual(result, ATTRACT_FUNCT.value * node_attraction)
+
+    def test_attraction_node_2(self):
+        """
+        Corner case: should not attract any node if the multiplier is 0.
+        """
+        node_attraction = 0
+        node = Node(np.array([-1]), ZERO, ZERO, 0, None, 0)
+        particle = create_particle(0, 0, 0, node_attraction)
+        result = particle.compute_attraction_force_to(node)
+        self.assertEqual(result, 0)
+
+    def test_attraction_marble_1(self):
+        """
+        Check if the marble_attraction multiplier is used to compute force to a
+        node.
+        """
+        marble_attraction = 0.5
+        # Position -1 ensures it will be pulled towards '+' x-direction
+        marble = Marble(np.array([-1]), ZERO, ZERO, 0, None, None)
+        particle = create_particle(0, 0, marble_attraction, 0)
+
+        result = particle.compute_attraction_force_to(marble)
+        self.assertEqual(result, ATTRACT_FUNCT.value * marble_attraction)
+
+    def test_attraction_marble_2(self):
+        """
+        Corner case: should not attract any node if the multiplier is 0.
+        """
+        marble_attraction = 0
+        marble = Marble(np.array([-1]), ZERO, ZERO, 0, None, None)
+        particle = create_particle(0, 0, marble_attraction, 0)
+        result = particle.compute_attraction_force_to(marble)
+        self.assertEqual(result, 0)
+
+    def test_attraction_wrong_type(self):
+        """
+        Corner case: can only compute attraction to Marbles and Nodes.
+        """
+        other = PhysicalParticle(np.array([-1]), ZERO, ZERO, 0, None)
+        particle = create_particle(0, 0, 0, 0)
+        self.assertRaises(
+            ValueError, particle.compute_attraction_force_to, other)
+
 
 def create_particle(marble_stiffness,
                     node_stiffness,
@@ -70,7 +130,7 @@ def create_particle(marble_stiffness,
     Simply attempt to create a StiffnessParticle with given parameters,
     and 0 or None for all other parameter values.
     """
-    return StiffnessParticle(ZERO, ZERO, ZERO, ZERO, None,
+    return StiffnessParticle(ZERO, ZERO, ZERO, ZERO, ATTRACT_FUNCT,
                              marble_stiffness=marble_stiffness,
                              node_stiffness=node_stiffness,
                              marble_attraction=marble_attraction,
