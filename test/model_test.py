d748a021daa1f44206704294c387030e5e245d26
nifrec <l.l.piree@student.tue.nl>
2020-11-12 19:41:49 +0100

    Elaborate test_make_timestep_2 and remove deprecated commented-out code

--- test/model_test.py
+++ test/model_test.py
@@ -94,7 +94,7 @@ class ModelTestCase(unittest.TestCase):
 
     def test_make_timestep_1(self):
         """
-        Base base: single moving Marble.
+        Base base: single moving Marble, with constant velocity.
         """
         marble = Marble(ZERO, np.array([10]), ZERO, 1, None, None, 0, 0, 0, 0)
         model = NenwinModel([], [marble])
@@ -105,6 +105,9 @@ class ModelTestCase(unittest.TestCase):
         self.assertTrue(check_close(marble.pos, expected_new_pos))
 
     def test_make_timestep_2(self):
+        """
+        Base case: initial stationairy Marble, gets attracted and accelerated.
+        """
         node = Node(pos=ZERO,
                     vel=ZERO,
                     acc=ZERO,
@@ -134,213 +137,9 @@ class ModelTestCase(unittest.TestCase):
         self.assertTrue(check_close(marble.vel, expected_vel, atol=0.01))
         self.assertTrue(check_close(marble.acc, -ATTRACT_FUNCT.value, atol=0.01))
 
-    # def test_model_run_1(self):
-    #     """
-    #     Base case: single marble and single node.
-    #     """
-    #     attract_funct = TestAttractionFunction()
-    #     zero_vector = np.array([0, 0])
-
-    #     node = Node(pos=zero_vector, vel=zero_vector, acc=zero_vector,
-    #                 mass=2, attraction_function=attract_funct, stiffness=1)
-    #     marble = Marble(pos=np.array([10, 0]), vel=zero_vector, acc=zero_vector,
-    #                     mass=1, attraction_function=attract_funct, datum=None)
-
-    #     model = NenwinModel([node],
-    #                         TEST_SIMULATION_STEP_SIZE,
-    #                         [marble])
-    #     # Simulate 1 unit time
-    #     num_steps = (1/TEST_SIMULATION_STEP_SIZE)
-    #     model.run(max_num_steps=num_steps)
-
-    #     # The node should not have changed at all
-    #     self.assertTrue(check_close(node.acc, zero_vector))
-    #     self.assertTrue(check_close(node.vel, zero_vector))
-    #     self.assertTrue(check_close(node.pos, zero_vector))
-
-    #     # The marble should have been pulled towards the node
-    #     difference = node.pos - marble.pos
-    #     direction = difference / np.linalg.norm(difference)
-    #     expected_acc = direction * attract_funct(node, marble) / marble.mass
-    #     expected_pos, expected_vel = runge_kutta_4_step(
-    #         pos=np.array([10, 0]), vel=zero_vector, acc=expected_acc,
-    #         duration=1)
-    #     self.assertTrue(check_close(marble.acc, expected_acc))
-    #     self.assertTrue(check_close(marble.vel, expected_vel))
-    #     self.assertTrue(check_close(marble.pos, expected_pos))
-
-    # def test_model_run_2(self):
-    #     """
-    #     Corner case: marble stationairy in equilibrium in middle of 2 nodes.
-    #     """
-    #     attract_funct = TestAttractionFunction()
-    #     zero_vector = np.array([0, 0])
-
-    #     node1 = Node(pos=zero_vector, vel=zero_vector, acc=zero_vector,
-    #                  mass=2, attraction_function=attract_funct, stiffness=1)
-    #     node2 = Node(pos=np.array([2, 2]), vel=zero_vector, acc=zero_vector,
-    #                  mass=2, attraction_function=attract_funct, stiffness=1)
-    #     marble = Marble(pos=np.array([1, 1]), vel=zero_vector, acc=zero_vector,
-    #                     mass=1, attraction_function=attract_funct, datum=None)
-
-    #     model = NenwinModel([node1, node2],
-    #                         TEST_SIMULATION_STEP_SIZE,
-    #                         [marble])
-    #     model.run(max_num_steps=1)
-
-    #     # No particle should have changed at all
-    #     self.assertTrue(check_close(node1.acc, zero_vector))
-    #     self.assertTrue(check_close(node1.vel, zero_vector))
-    #     self.assertTrue(check_close(node1.pos, zero_vector))
-
-    #     self.assertTrue(check_close(node2.acc, zero_vector))
-    #     self.assertTrue(check_close(node2.vel, zero_vector))
-    #     self.assertTrue(check_close(node2.pos, np.array([2, 2])))
-
-    #     self.assertTrue(check_close(marble.acc, zero_vector))
-    #     self.assertTrue(check_close(marble.vel, zero_vector))
-    #     self.assertTrue(check_close(marble.pos, np.array([1, 1])))
-
-    # def test_model_run_3(self):
-    #     """
-    #     Corner case: marble in equilibrium in middle of 2 nodes,
-    #     but with constant velocity in orthogonal direction.
-    #     """
-    #     attract_funct = TestAttractionFunction()
-    #     zero_vector = np.array([0, 0])
-
-    #     node1 = Node(pos=zero_vector, vel=zero_vector, acc=zero_vector,
-    #                  mass=2, attraction_function=attract_funct, stiffness=1)
-    #     node2 = Node(pos=np.array([2, 0]), vel=zero_vector, acc=zero_vector,
-    #                  mass=2, attraction_function=attract_funct, stiffness=1)
-    #     marble = Marble(pos=np.array([1, 0]), vel=np.array([1, 0]),
-    #                     acc=zero_vector, mass=1,
-    #                     attraction_function=attract_funct,
-    #                     datum=None)
-
-    #     model = NenwinModel([node1, node2],
-    #                         TEST_SIMULATION_STEP_SIZE,
-    #                         [marble])
-    #     model.run(max_num_steps=1)
-
-    #     self.assertTrue(check_close(node1.acc, zero_vector))
-    #     self.assertTrue(check_close(node1.vel, zero_vector))
-    #     self.assertTrue(check_close(node1.pos, zero_vector))
-
-    #     self.assertTrue(check_close(node2.acc, zero_vector))
-    #     self.assertTrue(check_close(node2.vel, zero_vector))
-    #     self.assertTrue(check_close(node2.pos, np.array([2, 0])))
-
-    #     expected_pos = np.array([1, 0]) \
-    #         + TEST_SIMULATION_STEP_SIZE*np.array([1, 0])
-    #     self.assertTrue(check_close(marble.acc, zero_vector))
-    #     self.assertTrue(check_close(marble.vel, np.array([1, 0])))
-    #     self.assertTrue(check_close(marble.pos, expected_pos))
-
-    # def test_model_run_4(self):
-    #     """
-    #     Base case: marble attracted by 2 nodes in opposite directions.
-    #     """
-    #     attract_funct = TestAttractionFunction()
-    #     zero_vector = np.array([0, 0])
-
-    #     node1 = Node(pos=np.array([0, 1]),
-    #                  vel=zero_vector,
-    #                  acc=zero_vector,
-    #                  mass=2,
-    #                  attraction_function=attract_funct,
-    #                  stiffness=1)
-    #     node2 = Node(pos=np.array([0, -1]),
-    #                  vel=zero_vector,
-    #                  acc=zero_vector,
-    #                  mass=2,
-    #                  attraction_function=attract_funct,
-    #                  stiffness=1)
-    #     marble = Marble(pos=np.array([1, 0]),
-    #                     vel=zero_vector,
-    #                     acc=zero_vector,
-    #                     mass=1,
-    #                     attraction_function=attract_funct,
-    #                     datum=None)
-
-    #     model = NenwinModel([node1, node2],
-    #                         TEST_SIMULATION_STEP_SIZE,
-    #                         [marble])
-    #     model.run(max_num_steps=1)
-
-    #     self.assertTrue(check_close(node1.acc, zero_vector))
-    #     self.assertTrue(check_close(node1.vel, zero_vector))
-    #     self.assertTrue(check_close(node1.pos, np.array([0, 1])))
-
-    #     self.assertTrue(check_close(node2.acc, zero_vector))
-    #     self.assertTrue(check_close(node2.vel, zero_vector))
-    #     self.assertTrue(check_close(node2.pos, np.array([0, -1])))
-
-    #     difference1 = node1.pos - marble.pos
-    #     direction1 = difference1 / np.linalg.norm(difference1)
-    #     difference2 = node2.pos - marble.pos
-    #     direction2 = difference2 / np.linalg.norm(difference2)
-    #     # Newton's Second Law:
-    #     expected_acc = np.array([0, 0])
-    #     expected_pos, expected_vel = runge_kutta_4_step(
-    #         pos=np.array([1, 0]), vel=zero_vector, acc=expected_acc,
-    #         duration=TEST_SIMULATION_STEP_SIZE)
-    #     self.assertTrue(check_close(marble.acc, expected_acc))
-    #     self.assertTrue(check_close(marble.vel, expected_vel))
-    #     self.assertTrue(check_close(marble.pos, expected_pos))
-
-    # def test_model_run_5(self):
-    #     """
-    #     Base case: single marble and a single movable node.
-    #     (stiffness != 1)
-    #     """
-    #     attract_funct = TestAttractionFunction()
-    #     zero_vector = np.array([0, 0])
-    #     stiffness = 0.5
-
-    #     node = Node(pos=zero_vector,
-    #                 vel=zero_vector,
-    #                 acc=zero_vector,
-    #                 mass=2,
-    #                 attraction_function=attract_funct,
-    #                 stiffness, stiffness, 1, 1)
-    #     marble = Marble(pos=np.array([1, 0]),
-    #                     vel=zero_vector,
-    #                     acc=zero_vector,
-    #                     mass=1,
-    #                     attraction_function=attract_funct,
-    #                     datum=None)
-
-    #     model = NenwinModel([node],
-    #                         TEST_SIMULATION_STEP_SIZE,
-    #                         [marble])
-    #     # Simulate 1 unit time
-    #     num_steps = (1/TEST_SIMULATION_STEP_SIZE)
-    #     model.run(max_num_steps=num_steps)
-
-    #     difference = marble.pos - node.pos
-    #     direction_node = difference / np.linalg.norm(difference)
-    #     expected_acc_node = stiffness * direction_node \
-    #         * attract_funct(node, marble) / node.mass #TODO: removing node.mass here creates very different result!
-    #     self.assertEqual(node.mass, 2)
-    #     expected_pos_node, expected_vel_node = runge_kutta_4_step(
-    #         pos=zero_vector, vel=zero_vector, acc=expected_acc_node,
-    #         duration=1)
-    #     self.assertTrue(check_close(node.acc, expected_acc_node))
-    #     self.assertTrue(check_close(node.vel, expected_vel_node))
-    #     self.assertTrue(check_close(node.pos, expected_pos_node))
-
-    #     difference = -difference
-    #     direction_marble = difference / np.linalg.norm(difference)
-    #     expected_acc_marble = direction_marble \
-    #         * attract_funct(node, marble)
-    #     expected_pos_marble, expected_vel_marble = runge_kutta_4_step(
-    #         pos=np.array([1, 0]), vel=zero_vector, acc=expected_acc_marble,
-    #         duration=1)
-    #     self.assertTrue(check_close(marble.acc, expected_acc_marble))
-    #     self.assertTrue(check_close(marble.vel, expected_vel_marble))
-    #     self.assertTrue(check_close(marble.pos, expected_pos_marble))
-
+        self.assertTrue(check_close(node.pos, ZERO))
+        self.assertTrue(check_close(node.vel, ZERO))
+        self.assertTrue(check_close(node.acc, ZERO))
 
 def generate_dummy_marble() -> Marble:
     """
