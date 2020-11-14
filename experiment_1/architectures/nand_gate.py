1b485b3a09966fb3007d07fd3300c88dc708bf09
nifrec <l.l.piree@student.tue.nl>
2020-11-14 12:16:14 +0100

    Adapt the NAND-gate simulation to the StiffnessParticle & Simulation overhauls

--- experiment_1/architectures/nand_gate.py
+++ experiment_1/architectures/nand_gate.py
@@ -25,16 +25,29 @@ Simulation of NAND-gates using Nenwin architectures.
 import numpy as np
 from typing import Tuple
 
-from experiment_1.model import NumMarblesEatenAsOutputModel
-from experiment_1.node import Node, MarbleEaterNode
-from experiment_1.marble import Marble
-from experiment_1.attraction_functions.attraction_functions import Gratan
+from experiment_1.simulation import Simulation
+from experiment_1.model import NenwinModel
+from experiment_1.stiffness_particle import Node, Marble
+from experiment_1.marble_eater_node import MarbleEaterNode
 from experiment_1.attraction_functions.attraction_functions import NewtonianGravity
 from experiment_1.visualization import NenwinVisualization
+from experiment_1.output_reader import NumMarblesOutputReader
 
 BOOL_TO_MASS = {True: 100, False: -100}
 ZERO = np.array([0, 0])
 ATTRACTION_FUNCTION = NewtonianGravity()
+STIFFNESS_AND_ATTRACTION = {
+    "marble_stiffness": 1,
+    "node_stiffness": 0,
+    "marble_attraction": 1,
+    "node_attraction": 0
+}
+STEP_SIZE = 0.001
+
+class MockPipe:
+
+    def poll(self):
+        return None
 
 def nand_gate(input_1: bool, input_2: bool) -> bool:
     """
@@ -45,12 +58,11 @@ def nand_gate(input_1: bool, input_2: bool) -> bool:
     print("Press ESC after the simulation ended to print NAND gate output")
     marble1, marble2 = __generate_nand_marbles(input_1, input_2)
     eater, wastebin_eater = __generate_nand_nodes()
-    model = NumMarblesEatenAsOutputModel([eater, wastebin_eater],
-                                         0.001,
-                                         [marble1, marble2],)
-    visualization = NenwinVisualization((500, 500), model, 10)
-    visualization.run(10)
-    output = model._produce_outputs()
+    model = NenwinModel([eater, wastebin_eater], [marble1, marble2],)
+    simulation = Simulation(model, None, None, MockPipe())
+    visualization = NenwinVisualization((500, 500), simulation, model, 10)
+    visualization.run(10, STEP_SIZE)
+    output = NumMarblesOutputReader().read_output(model)
 
     print("NAND output:")
     if output[0] == 2:
@@ -58,34 +70,38 @@ def nand_gate(input_1: bool, input_2: bool) -> bool:
     else:
         print(True)
 
+
 def __generate_nand_marbles(input_1: bool, input_2: bool) -> Tuple[Marble]:
     marble1 = Marble(np.array([20, 10]),
                      vel=ZERO, acc=ZERO, mass=BOOL_TO_MASS[input_1],
                      attraction_function=ATTRACTION_FUNCTION,
-                     datum=input_1)
+                     datum=input_1,
+                     **STIFFNESS_AND_ATTRACTION)
 
     marble2 = Marble(np.array([20, 30]),
                      vel=ZERO, acc=ZERO, mass=BOOL_TO_MASS[input_2],
                      attraction_function=ATTRACTION_FUNCTION,
-                     datum=input_2)
+                     datum=input_2,
+                     **STIFFNESS_AND_ATTRACTION)
     return marble1, marble2
+
 
 def __generate_nand_nodes() -> Tuple[MarbleEaterNode]:
     eater = MarbleEaterNode(pos=np.array([5, 20]),
                             vel=ZERO,
                             acc=ZERO,
                             mass=10,
-                            stiffness=1,
                             attraction_function=ATTRACTION_FUNCTION,
-                            radius=5)
+                            radius=5,
+                            **STIFFNESS_AND_ATTRACTION)
 
     wastebin_eater = MarbleEaterNode(pos=np.array([35, 20]),
                                      vel=ZERO,
                                      acc=ZERO,
                                      mass=-10,
-                                     stiffness=1,
                                      attraction_function=ATTRACTION_FUNCTION,
-                                     radius=5)
+                                     radius=5,
+                                     **STIFFNESS_AND_ATTRACTION)
     return eater, wastebin_eater
 
 
