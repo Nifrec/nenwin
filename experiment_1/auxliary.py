76e9313113f37c91e2ad48dda5f8235b24668e88
nifrec <l.l.piree@student.tue.nl>
2020-11-22 20:34:12 +0100

    Add helper function to generate dict for stiffness/attraction arguments of Node

--- experiment_1/auxliary.py
+++ experiment_1/auxliary.py
@@ -8,8 +8,23 @@ October 2020
 Multi-purpose auxiliary functions.
 """
 import numpy as np
+from typing import Dict
+
 from experiment_1.particle import Particle
+
 
 def distance(p1: Particle, p2: Particle) -> float:
     difference = p1.pos - p2.pos
     return np.linalg.norm(difference)
+
+
+def generate_stiffness_dict(marble_stiffness: float,
+                            node_stiffness: float,
+                            marble_attraction: float,
+                            node_attraction: float) -> Dict[str, float]:
+    return {
+        "marble_stiffness": marble_stiffness,
+        "node_stiffness": node_stiffness,
+        "marble_attraction": marble_attraction,
+        "node_attraction": node_attraction
+    }
