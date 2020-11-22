53bec0085c08c910378bdc5534e76eb01d4202e3
nifrec <l.l.piree@student.tue.nl>
2020-11-22 20:34:33 +0100

    Add plot for threshold gravity

--- experiment_1/attraction_functions/attraction_functions_plots.py
+++ experiment_1/attraction_functions/attraction_functions_plots.py
@@ -5,17 +5,28 @@ the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 Author: Lulof Pirée
 October 2020
 
-Plot of Newton's gravity formula, simplified to 1/r, and a similar-shaped
+Plot of gravity functions such as
+Newton's gravity formula, simplified to 1/r, and a similar-shaped
 non-polynomial function that has a finite limit at r -> 0.
 """
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D, axes3d
 import numpy as np
+from experiment_1.attraction_functions.attraction_functions import NewtonianGravity
+from experiment_1.attraction_functions.attraction_functions import Gratan
+
+def clipped_gravity(x: np.ndarray):
+    x = x.copy()
+    x[abs(x) > 3] = 0
+    x[x != 0] = abs(1/x[x != 0]**2)
+    return x
+
 
 X = np.concatenate([np.arange(-100, 0, 1),[-0.9, 0.9], np.arange(1, 100, 1)])
 X = np.concatenate([np.arange(-10, 0, 0.1), np.arange(0.1, 10.1, 0.1)])
-plt.plot(X, 0.2*abs(1/X**2), label="Absolute value of gravity")
-plt.plot(X, 1 - abs(np.tanh(X)), label="$1 - |\\tanh(x)|$")
+# plt.plot(X, 0.2*abs(1/X**2), label="Absolute value of gravity")
+plt.plot(X, 0.2*clipped_gravity(X), label="clipped_gravity")
+# plt.plot(X, 1 - abs(np.tanh(X)), label="$1 - |\\tanh(x)|$")
 plt.legend()
 plt.xlabel("Distance")
 plt.ylabel("Attraction strength")
