5164f55bbabdb620fa1385197875ce346c0cb4f4
nifrec <l.l.piree@student.tue.nl>
2020-12-02 19:23:15 +0100

    Draft for MarbleEmitterNode and MarbleEmitter

---
+++ experiment_1/marble_emitter_node.py
@@ -0,0 +1,92 @@
+"""
+Nenwin-project (NEural Networks WIthout Neurons) for
+the AI Honors Academy track 2020-2021 at the TU Eindhoven.
+
+Author: Lulof Pirée
+October 2020
+
+A variant of the Node that can create new Marbles, after having absorbed
+sufficient other Marbles.
+"""
+from __future__ import annotations
+from typing import Optional
+import numpy as np
+import abc
+
+from experiment_1.marble_eater_node import MarbleEaterNode
+from experiment_1.stiffness_particle import Marble, Node
+
+
+class MarbleEmitterNode(MarbleEaterNode):
+    """
+    A variant of MarbleEaterNodes that also can generate new Marbles.
+    """
+
+    def __init__(self,
+                 pos: np.ndarray,
+                 vel: np.ndarray,
+                 acc: np.ndarray,
+                 mass: float,
+                 attraction_function: callable,
+                 marble_stiffness: float,
+                 node_stiffness: float,
+                 marble_attraction: float,
+                 node_attraction: float,
+                 radius: float,
+                 emitter: Emitter):
+        super().__init__(pos, vel, acc, mass,
+                         attraction_function,
+                         marble_stiffness,
+                         node_stiffness,
+                         marble_attraction,
+                         node_attraction,
+                         radius)
+        self.__emit_interval = emit_interval
+        self.__stored_mass = 0
+
+    def eat(self, marble: Marble):
+        self.__stored_mass += marble.mass
+        super().eat(marble)
+
+
+class Emitter(abc.ABC):
+
+    def __init__(self,
+                 prototype: Node,
+                 delay: float,
+                 stored_mass: Optional[float] = 0):
+        self.__stored_mass = stored_mass
+        self.__delay = delay
+        self.__time_since_last_emit = 0
+
+    def emit(self) -> Node:
+        if self.__time_since_last_emit >= self.__delay:
+            self.__time_since_last_emit = 0
+            return self._create_particle()
+        else:
+            raise RuntimeError("Cannot emit, delay not completely passed")
+
+    @abc.abstractmethod
+    def _create_particle(self) -> Node:
+        pass
+
+    def eat_mass(self, mass: float):
+        self.__stored_mass += mass
+
+    def register_time_passed(self, time_passed: float):
+        self.__time_since_last_emit += time_passed
+ 
+
+class MarbleEmitter(Emitter):
+
+    def __init__(self,
+                 prototype: Marble,
+                 delay: flat,
+                 stored_mass: Optional[float] = 0):
+        if not isinstance(prototype, Marble):
+            raise ValueError("Prototype of MarbleEmitter must be a Marble")
+        super().__init__(prototype, delay, stored_mass)
+        self.__prototype = prototype.copy()
+
+    def _create_particle(self) -> Marble:
+        return self.__prototype.copy()
