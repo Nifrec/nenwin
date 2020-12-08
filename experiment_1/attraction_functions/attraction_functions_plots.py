a21d4dd9cfd9b76f1e5f59b45c9cb2efc6929fd2
nifrec <l.l.piree@student.tue.nl>
2020-12-08 18:46:12 +0100

    Add missing licence headers

--- experiment_1/attraction_functions/attraction_functions_plots.py
+++ experiment_1/attraction_functions/attraction_functions_plots.py
@@ -5,6 +5,21 @@ the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 Author: Lulof Pirée
 October 2020
 
+Copyright (C) 2020 Lulof Pirée, Teun Schilperoort
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 Plot of gravity functions such as
 Newton's gravity formula, simplified to 1/r, and a similar-shaped
 non-polynomial function that has a finite limit at r -> 0.
--- test/visualization_demo.py
+++ test/visualization_demo.py
@@ -5,6 +5,21 @@ the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 Author: Lulof Pirée
 October 2020
 
+Copyright (C) 2020 Lulof Pirée, Teun Schilperoort
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 Demonstration of the visualization of a Nenwin model.
 """
 import numpy as np
