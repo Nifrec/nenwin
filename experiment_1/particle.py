Fix gradients in copy() for PhysicalParticle

experiment_1/particle.py
@@ -228,11 +228,19 @@ class InitialValueParticle(Particle):
 
     def copy(self) -> InitialValueParticle:
         output = InitialValueParticle(self.init_pos, self.init_vel, self.init_acc)
-        output.init_pos = self.__init_pos
-        output.init_vel = self.__init_vel
-        output.init_acc = self.__init_acc
+        output.adopt_parameters(self)
 
-        return 
+        return output
+
+    def adopt_parameters(self, source: InitialValueParticle):
+        """
+        Sets the initial position, velocity and acceleration
+        to the same object (so by reference) to the init pos, vel and acc
+        of [source].
+        """
+        self.set_init_pos(source.init_pos)
+        self.set_init_vel(source.init_vel)
+        self.set_init_acc(source.init_acc)
 
 class PhysicalParticle(InitialValueParticle):
     """
@@ -253,11 +261,16 @@ class PhysicalParticle(InitialValueParticle):
 
     @property
     def mass(self) -> torch.Tensor:
-        return self.__mass.clone()
+        return self.__mass
 
-    @mass.setter
-    def mass(self, new_mass) -> torch.Tensor:
-        self.__mass = create_param(new_mass, device=self.device)
+    def set_mass(self, new_mass) -> torch.Tensor:
+        """
+        Directly change the mass *by reference*.
+        This is the only way to ensure compuational graphs will also be set.
+        """
+        if not isinstance(new_mass, nn.Parameter):
+            raise ValueError("New mass should be a torch.nn.Paratemer")
+        self.__mass = new_mass
 
     def __repr__(self) -> str:
         output = super().__repr__()
@@ -299,11 +312,17 @@ class PhysicalParticle(InitialValueParticle):
         return direction * self._attraction_function(self, other)
 
     def copy(self) -> PhysicalParticle:
-        return PhysicalParticle(self.init_pos,
+        output = PhysicalParticle(self.init_pos,
                                 self.init_vel,
                                 self.init_acc,
                                 self.mass,
                                 self._attraction_function)
+        output.adopt_parameters(self)
+        return output
+
+    def adopt_parameters(self, source: PhysicalParticle):
+        super().adopt_parameters(source)
+        self.set_mass(source.mass)
 
 
 def create_param(vector: Union[np.ndarray, torch.Tensor, float],

test/physical_particle_test.py
@@ -32,8 +32,7 @@ from experiment_1.attraction_functions.attraction_functions \
     import NewtonianGravity
 from experiment_1.attraction_functions.attraction_functions import Gratan
 from experiment_1.constants import DEVICE
-from test_aux import NUMERICAL_ABS_ACCURACY_REQUIRED
-from test_aux import runge_kutta_4_step
+from test_aux import check_close
 
 
 class PhysicalParticleTestCase(unittest.TestCase):
@@ -47,9 +46,8 @@ class PhysicalParticleTestCase(unittest.TestCase):
 
         self.assertEqual(particle.mass, mass)
 
-        # mass2 = torch.tensor(10, dtype=torch.float)
-        mass2 = 10
-        particle.mass = mass2
+        mass2 = torch.nn.Parameter(torch.tensor([10.0]), requires_grad=True)
+        particle.set_mass(mass2)
 
         self.assertEqual(particle.mass, mass2)
 
@@ -239,6 +237,26 @@ class PhysicalParticleTestCase(unittest.TestCase):
         self.assertTrue(torch.allclose(pos, copy.pos))
         self.assertEqual(mass, copy.mass)
         self.assertTrue(attraction_funct is copy._attraction_function)
+
+    def test_copy_with_mass_grad(self):
+        """
+        Gradients of mass should be copied as well.
+        """
+        pos = torch.tensor([1], dtype=torch.float)
+        vel = torch.tensor([2], dtype=torch.float)
+        acc = torch.tensor([3], dtype=torch.float)
+        mass = 4
+        attraction_funct = Gratan()
+        some_particle = PhysicalParticle(pos, vel, acc, mass, attraction_funct)
+
+        some_tensor = torch.tensor([4.0], requires_grad=True)
+        loss = torch.sum(some_particle.mass + some_tensor)
+        loss.backward()
+        self.assertIsNotNone(some_particle.mass.grad)
+        self.assertIsNotNone(some_tensor.grad)
+
+        another_particle = some_particle.copy()
+        self.assertIs(some_particle.mass.grad, another_particle.mass.grad)
 
     def test_parameters(self):
         pos = torch.tensor([1], dtype=torch.float)