64ea0e7a88d7ed7991bdcb398fe3924388dbfb0c
nifrec <lulof.piree@zoho.com>
2021-03-10 21:31:46 +0100

    Update licence headers

--- nenwin/marble_emitter_node.py
+++ nenwin/marble_emitter_node.py
@@ -5,6 +5,24 @@ the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 Author: Lulof Pirée
 March 2021
 
+Copyright (C) 2021 Lulof Pirée, Teun Schilperoort
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Class representing the state of a simulation:
+    keeps track of the nodes, marbles, and advances timesteps.
+
 A variant of the Node that can create new Marbles, after having absorbed
 sufficient other Marbles.
 """
