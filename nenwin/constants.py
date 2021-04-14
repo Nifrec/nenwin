79cb4e24ebd66d6b365e1956e25a5b1fe483b63d
nifrec <lulof.piree@zoho.com>
2021-04-14 11:25:53 +0200

    Fix dates

--- nenwin/constants.py
+++ nenwin/constants.py
@@ -3,9 +3,9 @@ Nenwin-project (NEural Networks WIthout Neurons) for
 the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 
 Author: Lulof Pirée
-October 2020
+April 2021
 
-Copyright (C) 2020 Lulof Pirée, Teun Schilperoort
+Copyright (C) 2021 Lulof Pirée, Teun Schilperoort
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
