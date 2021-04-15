38a680ada3a1b34c1075b082c3f6f31df1f99121
nifrec <lulof.piree@zoho.com>
2021-04-15 11:48:05 +0200

    Cleanup and docstring

--- nenwin/test/input_placer_test.py
+++ nenwin/test/input_placer_test.py
@@ -5,7 +5,7 @@ the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 Author: Teun Schilperoort
 March 2021
 
-Copyright (C) 2020 Lulof Pirée, Teun Schilperoort
+Copyright (C) 2021 Lulof Pirée, Teun Schilperoort
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
@@ -20,17 +20,11 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Unit tests for inputplacer
+Unit tests for PhiInputPlacer.
 """
-import abc
 import unittest
-import numpy as np
-from typing import Iterable, Optional, Sequence, List
-import math
 import torch
 
-from nenwin.node import Marble
-from nenwin.attraction_functions.attraction_functions import NewtonianGravity
 from nenwin.input_placer import PhiInputPlacer
 
 class InputPlacerTestCase(unittest.TestCase):
