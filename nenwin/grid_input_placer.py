35a2a70c3610499f76ca58abda1f8975d1b8efe7
nifrec <lulof.piree@zoho.com>
2021-04-16 11:52:13 +0200

    Implement GridInputPlacer (& extend docstring)

--- nenwin/grid_input_placer.py
+++ nenwin/grid_input_placer.py
@@ -23,35 +23,88 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 InputPlacer variant that maps a 2D array (matrix) to a set of Marbles
 positioned in a grid.
 Also generalizes to higher dimensions.
-The grid entried correspond to the entries 
+The grid entried correspond to the entries
 in the matrix at the corresponding index.
 """
-from typing import Iterable
+from __future__ import annotations
+from typing import Iterable, Sequence, Set
 import numpy as np
 import torch
+import itertools
 
 from nenwin.input_placer import InputPlacer
 from nenwin.all_particles import Marble
+from nenwin.attraction_functions.attraction_functions import ThresholdGravity
+from nenwin.auxliary import generate_stiffness_dict
+
+GRID_MARBLE_STIFFNESSES = generate_stiffness_dict(marble_stiffness=1,
+                            node_stiffness=0,
+                            marble_attraction=0,
+                            node_attraction=1)
 
 class GridInputPlacer(InputPlacer):
     """
     InputPlacer variant that maps a 2D array (matrix) to a set of Marbles
     positioned in a regular grid.
     Also generalizes to higher dimensions.
-    The grid entried correspond to the entries 
+    The grid entried correspond to the entries
     in the matrix at the corresponding index.
 
     This most basic GridInputPlacer only looks at position,
     and ignores the values of the input-array entries.
     Subclasses can override hooks to make a more complex mapping.
+
+    The Marbles have the following fixed properties:
+        * ThresholdGravity attraction function.
+        * marble_stiffness = 1
+        * node_stiffness = 0
+        * marble_attraction = 0
+        * node_attraction = 1
+
+    And the following properties are based on input values, 
+    in a mapping defined by hook methods:
+        * mass (default: 1)
+        * vel (default: zero vector)
+        * acc (default: zero vector)
+        * threshold-gravity radius (default: 100)
     """
 
-    def marblelize_data(self, input_data: Iterable[object]) -> Iterable[Marble]:
-        return super().marblelize_data(input_data)
+    def marblelize_data(self, input_data: np.ndarray | torch.Tensor
+                        ) -> Iterable[Marble]:
+        grid_cell_size = self.input_region_sizes / input_data.shape
+
+        positions_per_dim = []
+
+        for dim in range(self.num_dims):
+            offset = self.input_pos[dim] + grid_cell_size[dim] / 2
+            positions = [offset + grid_cell_size[dim]*cell
+                         for cell in range(input_data.shape[dim])]
+            positions_per_dim.append(positions)
+
+        positions = itertools.product(*positions_per_dim)
+        return self.__create_marbles(positions, input_data.reshape(-1))
+
+    def __create_marbles(self, positions: Iterable[Sequence[float]],
+                         values: Iterable[float]) -> Set[Marble]:
+        marbles = set()
+        for pos, value in zip(positions, values):
+            assert isinstance(value, float)
+            pos = torch.tensor(pos, dtype=torch.float)
+            vel = self._map_value_to_vel(value)
+            acc = self._map_value_to_acc(value)
+            mass = self._map_value_to_mass(value)
+            attract_radius = self._map_value_to_gravity_radius(value)
+            marble = Marble(pos, vel, acc, mass,
+                            ThresholdGravity(attract_radius),
+                            datum=value,
+                            **GRID_MARBLE_STIFFNESSES
+                            )
+            marbles.add(marble)
+        return marbles
 
     def _map_value_to_mass(self, value: float) -> float:
         """
-        Hook method used by marblelize_data() 
+        Hook method used by marblelize_data()
         to find the *mass* of the Marble
         that is to be associated with with a certain entry (value)
         in the given input_data.
@@ -60,7 +113,7 @@ class GridInputPlacer(InputPlacer):
 
     def _map_value_to_vel(self, value: float) -> torch.Tensor:
         """
-        Hook method used by marblelize_data() 
+        Hook method used by marblelize_data()
         to find the *velocity vector* of the Marble
         that is to be associated with with a certain entry (value)
         in the given input_data.
@@ -69,12 +122,23 @@ class GridInputPlacer(InputPlacer):
 
     def _map_value_to_acc(self, value: float) -> torch.Tensor:
         """
-        Hook method used by marblelize_data() 
+        Hook method used by marblelize_data()
         to find the *acceleration vector* of the Marble
         that is to be associated with with a certain entry (value)
         in the given input_data.
         """
         return torch.zeros(self.num_dims, dtype=torch.float)
+
+    def _map_value_to_gravity_radius(self, value: float) -> float:
+        """
+        Hook method used by marblelize_data()
+        to find the *gravity threshold radius* of the Marble's
+        ThresholdGravityAttractionFunction.
+        So that the radius corresponds correctly to a certain entry (value)
+        in the given input_data.
+        """
+        return torch.zeros(self.num_dims, dtype=torch.float)
+
 
 class MassGridInputPlacer(GridInputPlacer):
     """
@@ -85,7 +149,7 @@ class MassGridInputPlacer(GridInputPlacer):
 
     def _map_value_to_mass(self, value: float) -> float:
         """
-        Hook method used by marblelize_data() 
+        Hook method used by marblelize_data()
         to find the *mass* of the Marble
         that is to be associated with with a certain entry (value)
         in the given input_data.
