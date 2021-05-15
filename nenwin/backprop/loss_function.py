478108da1d52cf32c76ca6b997a3ea453ff32720
nifrec <lulof.piree@zoho.com>
2021-05-15 09:11:53 +0200

    Update date

--- nenwin/backprop/loss_function.py
+++ nenwin/backprop/loss_function.py
@@ -3,7 +3,7 @@ Nenwin-project (NEural Networks WIthout Neurons) for
 the AI Honors Academy track 2020-2021 at the TU Eindhoven.
 
 Author: Lulof Pirée
-March 2021
+May 2021
 
 Copyright (C) 2021 Lulof Pirée, 
 
